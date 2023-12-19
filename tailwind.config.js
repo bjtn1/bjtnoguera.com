34ff1ed81e8035306e6918e5c36a5d7e7a6370b1
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-12-19 00:32:43 -0500

    Removed annoying google eslint config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      "black": "#16161e",
+      "white": "#c8d3f5",
+      "blue": "#82aaff",
+      "cyan": "#86e1fc",
+      "purple": "#fca7ea",
+      "magenta": "#c099ff",
+      "orange": "#ff966c",
+      "yellow": "#ffc777",
+      "green": "#c3e88d",
+      "red": "#ff757f",
+    },
     extend: {},
   },
   plugins: [],
