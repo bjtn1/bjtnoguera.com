8d661c414d038810dcbb68909552aeac43a85d41
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2023-12-31 02:23:57 -0500

    Figured out how to add rules for all instances of a specific tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,57 +26,7 @@ module.exports = {
       "red": "#ff757f",
       "teal": "#4fd6be",
     },
-    extend: {
-      // This is how to define custom colors for tailwindcss' "prose" class
-      // I don't know what the square brackets are for
-      // So far, I'm deleting the square brackets to "enable" the custom colors on any tag with the prose class
-      // To use these colors, you do 
-      // <div class="prose prose-maincolors">{{ markdown }}</div>
-      typography: ({ theme }) => ({
-        maincolors: {
-          css: {
-            '--tw-prose-body': theme('colors.white'),
-            '--tw-prose-headings': theme('colors.white-50'),
-            // '--tw-prose-lead': theme('colors.white[700]'),
-            '--tw-prose-links': theme('colors.blue'),
-            // '--tw-prose-bold': theme('colors.white[900]'),
-            // '--tw-prose-counters': theme('colors.white[600]'),
-            // '--tw-prose-bullets': theme('colors.white[400]'),
-            // '--tw-prose-hr': theme('colors.white[300]'),
-            '--tw-prose-quotes': theme('colors.white'),
-            '--tw-prose-quote-borders': theme('colors.black'),
-            // '--tw-prose-captions': theme('colors.white[700]'),
-            '--tw-prose-code': theme('colors.white'),
-            // '--tw-prose-pre-code': theme('colors.white[100]'),
-            // '--tw-prose-pre-bg': theme('colors.white[900]'),
-            // '--tw-prose-th-borders': theme('colors.white[300]'),
-            // '--tw-prose-td-borders': theme('colors.white[200]'),
-            // '--tw-prose-invert-body': theme('colors.white[200]'),
-            // '--tw-prose-invert-headings': theme('colors.white'),
-            // '--tw-prose-invert-lead': theme('colors.white[300]'),
-            // '--tw-prose-invert-links': theme('colors.white'),
-            // '--tw-prose-invert-bold': theme('colors.white'),
-            // '--tw-prose-invert-counters': theme('colors.white[400]'),
-            // '--tw-prose-invert-bullets': theme('colors.white[600]'),
-            // '--tw-prose-invert-hr': theme('colors.white[700]'),
-            // '--tw-prose-invert-quotes': theme('colors.white[100]'),
-            // '--tw-prose-invert-quote-borders': theme('colors.white[700]'),
-            // '--tw-prose-invert-captions': theme('colors.white[400]'),
-            // '--tw-prose-invert-code': theme('colors.white'),
-            // '--tw-prose-invert-pre-code': theme('colors.white[300]'),
-            // '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
-            // '--tw-prose-invert-th-borders': theme('colors.white[600]'),
-            // '--tw-prose-invert-td-borders': theme('colors.white[700]'),
-            'code::before': {
-              content: '""'
-            },
-            'code::after': {
-              content: '""'
-            }
-          },
-        },
-      }),
-    },
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
