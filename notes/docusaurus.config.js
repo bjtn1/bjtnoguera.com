c37196bd07d222b42f0f2d9da450d5db6b199d5f
Brandon J. T. Noguera <brandonjtnoguera+github@gmail.com>
2024-01-06 18:12:56 -0500

    Force darkmode (its cooler)

--- notes/docusaurus.config.js
+++ notes/docusaurus.config.js
@@ -81,7 +81,8 @@ const config = {
     },
     colorMode: {
       defaultMode: "dark",
-      respectPrefersColorScheme: true,
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
     },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
